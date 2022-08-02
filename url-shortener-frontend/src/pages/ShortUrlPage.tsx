f193a21d0fbe1d9b846ac7d3c7326a658dea2acd
lst <bbyojjok@gmail.com>
2022-08-02 11:23:36 +0900

    heroku deploy

--- url-shortener-frontend/src/pages/ShortUrlPage.tsx
+++ url-shortener-frontend/src/pages/ShortUrlPage.tsx
@@ -1,13 +1,9 @@
-import { Helmet } from 'react-helmet-async';
 import Wrapper from '../components/Wrapper';
 import ShortUrlContainer from '../containers/ShortUrlContainer';
 
 const HomePage = () => {
   return (
     <Wrapper>
-      <Helmet>
-        <title>Home | URL-SHORTENER</title>
-      </Helmet>
       <ShortUrlContainer />
     </Wrapper>
   );
