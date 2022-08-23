f7edba16c44b60929dae4b98bcf8254bf65e98a0
lst <bbyojjok@gmail.com>
2022-08-23 09:28:59 +0900

    modify backend shortBaseUrl

--- url-shortener-backend/src/api/url/url.ctrl.js
+++ url-shortener-backend/src/api/url/url.ctrl.js
@@ -12,8 +12,6 @@ POST /api/url
 export const create = async (ctx) => {
   const { originalUrl } = ctx.request.body;
 
-  console.log('ctx.request.header:', ctx.request.header);
-
   // validation
   const schema = Joi.object().keys({
     originalUrl: Joi.string().uri().required(),
@@ -33,10 +31,9 @@ export const create = async (ctx) => {
     return;
   }
 
-  const shortBaseUrl = `${ctx.request.header.origin}/`;
-
-  console.log('ctx.request.header.origin:', ctx.request.header.origin);
-  console.log('ctx.request.origin:', ctx.request.origin);
+  // const shortBaseUrl = `${ctx.request.header.origin}/`;
+  const shortBaseUrl = `${ctx.request.header['x-forwarded-proto']}://${ctx.request.header.host}/`;
+  console.log('shortBaseUrl:', shortBaseUrl);
 
   const urlCode = nanoid(10);
   const shortUrl = existUrl
